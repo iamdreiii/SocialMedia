55f8c94dc228ecf52b974eb1c02f3a171a4d1466
iamdreiii <marcandrei.gobison@gmail.com>
2023-01-16 19:21:06 +0800

    commit serializers

--- core/views.py
+++ core/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import exceptions
+from.serializers import UserSerializer
 
 class RegisterAPIView(APIView):
     def post(self, request):
-        return Response(request.data)
+        data = request.data
+        if data['password'] != data['password_confirm']:
+            raise exceptions.APIException('password not match')
+        serializer = UserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
