447a65664fddea7d36df741285582d122c783d60
iamdreiii <marcandrei.gobison@gmail.com>
2023-01-16 19:36:41 +0800

    commit serializer method

--- core/serializers.py
+++ core/serializers.py
@@ -5,3 +5,13 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model =  User
         fields = ['id', 'first_name', 'last_name', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+    def create(self, validate_data):
+        password = validate_data.pop('password', None)
+        instance = self.Meta.model(**validate_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
